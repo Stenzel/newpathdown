87f4e862806a4f76da5a0836abe4e0578a0450e8
Stefan Stenzel <stefan@ioptigan.com>
2019-03-09 11:08:00 +0100

    better window for filters

--- resampler.cpp
+++ resampler.cpp
@@ -1,7 +1,7 @@
 /*
  Resampling using different methods for up- and downsampling
  (c) 2019 Stefan Stenzel 
- stefan at ioptigan dot com	
+ stefan at ioptigan dot com 
 
  See license.txt for terms of use
 
@@ -15,132 +15,132 @@
 //set transposition in semitones
 void resampler::transpose(float semitone)
 {
-	setratio(exp2f(semitone / 12.0f));
+    setratio(exp2f(semitone / 12.0f));
 }
 
 //set resampling ratio
 void resampler::setratio(float newratio)
 {
-	ratio     = newratio;
-	downscale = 1.0f / ratio;
-	deltaup   = 0x1p25f * ratio;
-	deltadown = 0x1p25f / ratio;
-	
-	if(deltadown > 0x2000000 && state == DOWN) state = DOWN2UP;
-	if(deltaup   > 0x2000000 && state == UP)   state = UP2DOWN;
+    ratio     = newratio;
+    downscale = 1.0f / ratio;
+    deltaup   = 0x1p25f * ratio;
+    deltadown = 0x1p25f / ratio;
+    
+    if(deltadown > 0x2000000 && state == DOWN) state = DOWN2UP;
+    if(deltaup   > 0x2000000 && state == UP)   state = UP2DOWN;
 }
 
 // conventional approach, used for upsampling
 const float *resampler::up16(const float *src, float *dst)
 {
-	uint32_t ixsmp = (counter >> 25) & 0x07;
-	for(int i=0; i<16; i++)
-	{
-		counter += deltaup;
-		uint32_t ixnew = (counter >> 25) & 0x07; 
-		
-		//if(ixnew != ixsmp)	in[ixsmp] = *src++;
-		while(ixnew != ixsmp)							// this allows this routine to be uses for slight upsampling as well.
-		{
-			in[ixsmp] = *src++;
-			ixsmp = (ixsmp + 1) & 0x07;
-		}
+    uint32_t ixsmp = (counter >> 25) & 0x07;
+    for(int i=0; i<16; i++)
+    {
+        counter += deltaup;
+        uint32_t ixnew = (counter >> 25) & 0x07; 
+        
+        //if(ixnew != ixsmp)    in[ixsmp] = *src++;
+        while(ixnew != ixsmp)                           // this allows this routine to be uses for slight upsampling as well.
+        {
+            in[ixsmp] = *src++;
+            ixsmp = (ixsmp + 1) & 0x07;
+        }
 
-		uint32_t ixflt = (counter >> 17) & 0x0FF; 
-		float fraction = (counter & 0x1FFFF) * 0x1p-17f;
-		float *sinca = &Sinc[ixflt][0];
-		float *sincb = &Sinc[ixflt+1][0];
-	
-		float acca = 0.0f;
-		float accb = 0.0f;
-		for(int i=0; i<8; i++) 
-		{	
-			acca += in[ixsmp] * *sinca++;
-			accb += in[ixsmp] * *sincb++;
-			ixsmp = (ixsmp + 1) & 0x07;
-		}
-		*dst++ = acca + (accb - acca) * fraction;
-	}
-	return src;
+        uint32_t ixflt = (counter >> 17) & 0x0FF; 
+        float fraction = (counter & 0x1FFFF) * 0x1p-17f;
+        float *sinca = &Sinc[ixflt][0];
+        float *sincb = &Sinc[ixflt+1][0];
+    
+        float acca = 0.0f;
+        float accb = 0.0f;
+        for(int i=0; i<8; i++) 
+        {   
+            acca += in[ixsmp] * *sinca++;
+            accb += in[ixsmp] * *sincb++;
+            ixsmp = (ixsmp + 1) & 0x07;
+        }
+        *dst++ = acca + (accb - acca) * fraction;
+    }
+    return src;
 }
 
 //the new method for downsampling
 const float *resampler::down16(const float *src, float *dst)
 {
-	uint32_t ixsmp = (counter >> 25) & 0x07;
-	float *end = dst + 16;
-	do {
-		uint32_t ixflt = (counter >> 17) & 0x0FF; 
-		float fraction = (counter & 0x1FFFF) * 0x1p-17f;
-		float *sinca = &Sinc[ixflt][0];
-		float *sincb = &Sinc[ixflt+1][0];
-		float x = *src++ * downscale;
-	
-		float xb = x * fraction;
-		float xa = x - xb;
-	
-		for(int i=0; i<8; i++) 
-		{
-			out[ixsmp] += xa * *sinca++;
-			out[ixsmp] += xb * *sincb++;
-			ixsmp = (ixsmp + 1) & 0x07;
-		}
-		counter += deltadown;
-		uint32_t ixnew = (counter >> 25) & 0x07; 
-		if(ixnew != ixsmp)	
-		{
-			*dst++ = out[ixsmp];	
-			out[ixsmp] = 0.0f;
-			ixsmp = ixnew;
-		}
-	} while(dst < end);
-	return src;
+    uint32_t ixsmp = (counter >> 25) & 0x07;
+    float *end = dst + 16;
+    do {
+        uint32_t ixflt = (counter >> 17) & 0x0FF; 
+        float fraction = (counter & 0x1FFFF) * 0x1p-17f;
+        float *sinca = &Sinc[ixflt][0];
+        float *sincb = &Sinc[ixflt+1][0];
+        float x = *src++ * downscale;
+    
+        float xb = x * fraction;
+        float xa = x - xb;
+    
+        for(int i=0; i<8; i++) 
+        {
+            out[ixsmp] += xa * *sinca++;
+            out[ixsmp] += xb * *sincb++;
+            ixsmp = (ixsmp + 1) & 0x07;
+        }
+        counter += deltadown;
+        uint32_t ixnew = (counter >> 25) & 0x07; 
+        if(ixnew != ixsmp)  
+        {
+            *dst++ = out[ixsmp];    
+            out[ixsmp] = 0.0f;
+            ixsmp = ixnew;
+        }
+    } while(dst < end);
+    return src;
 }
 
 // used for fading in new version oer eight samples
 // cannot name things "new", so I used German 
 void fade(float *dst, float *alt, float *neu)
 {
-	for(int i=0; i<8; i++) *dst++ = *alt++;
-	
-	neu += 8;
-	
-	const float step = 1.0f/9.0f;
-	float sc = step;
-	for(int i=0; i<8; i++)
-	{
-		float a = *alt++;
-		*dst++ = a + (*neu++ - a) * sc;
-		sc += step;
-	}
+    for(int i=0; i<8; i++) *dst++ = *alt++;
+    
+    neu += 8;
+    
+    const float step = 1.0f/9.0f;
+    float sc = step;
+    for(int i=0; i<8; i++)
+    {
+        float a = *alt++;
+        *dst++ = a + (*neu++ - a) * sc;
+        sc += step;
+    }
 }
 
 //transition from downsampling to upsampling
 const float *resampler::down2up16(const float *src, float *dst)
 {
-	float neu[16];
-	uint32_t nextcount = ~counter;
-	down16(src,dst);
-	state = UP;
-	counter = nextcount; 
-	for(int i=0; i<8; i++) in[i] = 0.0f;
-	src = up16(src,neu);
-	fade(dst,dst,neu);
-	return src;
+    float neu[16];
+    uint32_t nextcount = ~counter;
+    down16(src,dst);
+    state = UP;
+    counter = nextcount; 
+    for(int i=0; i<8; i++) in[i] = 0.0f;
+    src = up16(src,neu);
+    fade(dst,dst,neu);
+    return src;
 }
 
 //transition from upsampling to downsampling
 const float *resampler::up2down16(const float *src, float *dst)
 {
-	float neu[16];
-	uint32_t nextcount = ~counter;
-	up16(src,dst);
-	state = DOWN;
-	counter = nextcount;  
-	for(int i=0; i<8; i++) out[i] = 0.0f;
-	src =  down16(src,neu);
-	fade(dst,dst,neu);
-	return src;
+    float neu[16];
+    uint32_t nextcount = ~counter;
+    up16(src,dst);
+    state = DOWN;
+    counter = nextcount;  
+    for(int i=0; i<8; i++) out[i] = 0.0f;
+    src =  down16(src,neu);
+    fade(dst,dst,neu);
+    return src;
 }
 
 //process a block of 16 samples
@@ -148,22 +148,25 @@ const float *resampler::up2down16(const float *src, float *dst)
 //returns pointer behind the last eaten sample 
 const float *resampler::process16(const float *src, float *dst)
 {
-	switch(state)
-	{
-		case UP:		return up16(src,dst);
-		case DOWN:		return down16(src,dst);
-		case UP2DOWN:	return up2down16(src,dst);
-		case DOWN2UP:	
-		default:		return down2up16(src,dst);		
-	}	
+    switch(state)
+    {
+        case UP:        return up16(src,dst);
+        case DOWN:      return down16(src,dst);
+        case UP2DOWN:   return up2down16(src,dst);
+        case DOWN2UP:   
+        default:        return down2up16(src,dst);      
+    }   
 }
+
+#define WCF0        0.344109006115
+#define WCF1       -0.508650393885
+#define WCF2        0.193119398005
+#define WCF3       -0.028578010236
+#define DILATION    0.83
 
 //build the windowed sinc filter table
 void resampler::buildsinc(void)
 {
-	#define DILATION 0.79561f
-
-    double sinc[8];
     double step  = M_PI * DILATION;
     double wstep = M_PI * 2.0/(8 + 1.0);
     
@@ -172,24 +175,20 @@ void resampler::buildsinc(void)
         double fraction = 1.0f - i * 1.0/256;
         double arg  = (fraction - 8 * 0.5) * step;
         double warg = (fraction + 0.5) * wstep;
-        
-        double sum = 0;
-        
+            
         for(int k=0; k<8; k++)
         {
             double  s = 1.0;
             if(arg) s = sin(arg)/arg;             // sinc - but avoid division by zero.
             
-            s *= 0.52 - 0.48 * cos(warg);         // window
-            sinc[k] = s;
-            sum  += s;                            // sum for gain correction
+            // Apply the Stenzel Window to minimize DC error 
+            double w = WCF0 + WCF1 * cos(warg) + WCF2 * cos(warg*2.0) + WCF3 * cos(warg*3.0);
+
+            Sinc[i][k] = s * w;                   // save windowed tap  
             
             arg  += step;                         // increment sinc arg
             warg += wstep;                        // increment window arg
         }
-        
-        double gain_correction = 1.0/sum;
-        for(int k=0; k<8; k++) Sinc[i][k] = sinc[k] * gain_correction;
     }
 }
 
@@ -198,14 +197,14 @@ float resampler::Sinc[256+1][8];
 //constructor...
 resampler::resampler()
 {
-	counter  = 0;
-	state = UP;
-	setratio(1.0f);
-	for(int i=0; i<8; i++) 
-	{
-		in[i]  = 0;
-		out[i] = 0;
-	}
-	buildsinc();
+    counter  = 0;
+    state = UP;
+    setratio(1.0f);
+    for(int i=0; i<8; i++) 
+    {
+        in[i]  = 0;
+        out[i] = 0;
+    }
+    buildsinc();
 }
 
